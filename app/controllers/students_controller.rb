cc166272d06c6eedea1026eefc3495963297e353
raaynaldo <raaynaldo@hotmail.com>
2020-10-29 23:44:35 -0400

    commit tree c8c1254273568e03a033187fd728eb1dab2e78ea

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,4 @@
 class StudentsController < ApplicationController
-  
   def index
     @students = Student.all
   end
@@ -9,9 +8,12 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
-  end
+    student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
 
+    redirect_to student_path(student.id)
+  end
 end
